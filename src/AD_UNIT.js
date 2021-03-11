c4aca39fb67f4b0421cfa22ee2786b8b3543f2a7
BGK <gorkemkizilok@gmail.com>
2021-03-11 22:25:13 +0300

    gitignore update

--- src/AD_UNIT.js
+++ src/AD_UNIT.js
@@ -1,7 +1,7 @@
-// import { TestIds } from '@react-native-firebase/admob';
+import { TestIds } from '@react-native-firebase/admob';
 
 // const AD_UNIT = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
-// const AD_UNIT = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-1527095319992073/5580597143';
-const AD_UNIT = 'ca-app-pub-1527095319992073/5580597143';
+const AD_UNIT = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-1527095319992073/5580597143';
+// const AD_UNIT = 'ca-app-pub-1527095319992073/5580597143';
 
 export default AD_UNIT;
